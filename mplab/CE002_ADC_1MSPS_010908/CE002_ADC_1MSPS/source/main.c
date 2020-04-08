21a8f91244fd076257bb2ad2f04aee71ae10580d
wattnotions <shaneormonde@gmail.com>
2020-04-08 21:02:14 +0100

    added example pic code as initial test

---
+++ mplab/CE002_ADC_1MSPS_010908/CE002_ADC_1MSPS/source/main.c
@@ -0,0 +1,84 @@
+/**********************************************************************
+* © 2005 Microchip Technology Inc.
+*
+* FileName:        main.c
+* Dependencies:    Header (.h) files if applicable, see below
+* Processor:       dsPIC30Fxxxx
+* Compiler:        MPLAB® C30 v3.00 or higher
+*
+* SOFTWARE LICENSE AGREEMENT:
+* Microchip Technology Incorporated ("Microchip") retains all ownership and 
+* intellectual property rights in the code accompanying this message and in all 
+* derivatives hereto.  You may use this code, and any derivatives created by 
+* any person or entity by or on your behalf, exclusively with Microchip's
+* proprietary products.  Your acceptance and/or use of this code constitutes 
+* agreement to the terms and conditions of this notice.
+*
+* CODE ACCOMPANYING THIS MESSAGE IS SUPPLIED BY MICROCHIP "AS IS".  NO 
+* WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED 
+* TO, IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A 
+* PARTICULAR PURPOSE APPLY TO THIS CODE, ITS INTERACTION WITH MICROCHIP'S 
+* PRODUCTS, COMBINATION WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+*
+* YOU ACKNOWLEDGE AND AGREE THAT, IN NO EVENT, SHALL MICROCHIP BE LIABLE, WHETHER 
+* IN CONTRACT, WARRANTY, TORT (INCLUDING NEGLIGENCE OR BREACH OF STATUTORY DUTY), 
+* STRICT LIABILITY, INDEMNITY, CONTRIBUTION, OR OTHERWISE, FOR ANY INDIRECT, SPECIAL, 
+* PUNITIVE, EXEMPLARY, INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, FOR COST OR EXPENSE OF 
+* ANY KIND WHATSOEVER RELATED TO THE CODE, HOWSOEVER CAUSED, EVEN IF MICROCHIP HAS BEEN 
+* ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT 
+* ALLOWABLE BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY RELATED TO 
+* THIS CODE, SHALL NOT EXCEED THE PRICE YOU PAID DIRECTLY TO MICROCHIP SPECIFICALLY TO 
+* HAVE THIS CODE DEVELOPED.
+*
+* You agree that you are solely responsible for testing the code and 
+* determining its suitability.  Microchip has no obligation to modify, test, 
+* certify, or support the code.
+*
+* REVISION HISTORY:
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* Author            Date      Comments on this revision
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* Hrushikesh Vasuki 07/29/05  First release of source file
+*
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*
+* ADDITIONAL NOTES:
+*
+*
+**********************************************************************/
+
+#include "p30fxxxx.h"
+
+//Macros for Configuration Fuse Registers:
+//Invoke macros to set up  device configuration fuse registers.
+//The fuses will select the oscillator source, power-up timers, watch-dog
+//timers, BOR characteristics etc. The macros are defined within the device
+//header files. The configuration fuse registers reside in Flash memory.
+_FOSC(CSW_FSCM_OFF & XT_PLL16);  //Run this project using an external crystal
+                                //routed via the PLL in 16x multiplier mode
+                                //For the 7.3728 MHz crystal we will derive a
+                                //throughput of 7.3728e+6*16/4 = 29.4 MIPS(Fcy)
+                                //,~33.9 nanoseconds instruction cycle time(Tcy).
+_FWDT(WDT_OFF);                 //Turn off the Watch-Dog Timer.
+_FBORPOR(MCLR_EN & PWRT_OFF);   //Enable MCLR reset pin and turn off the
+                                //power-up timers.
+_FGS(CODE_PROT_OFF);            //Disable Code Protection
+
+
+
+int main (void);
+
+int main (void)
+{
+        ADC_Init();             //Initialize the A/D converter
+
+        while (1);              //Loop Endlessly - Execution is interrupt driven
+                                //from this point on.
+        return 0;
+}
+
+
+
+
+
+
---
+++ mplab/dsPIC30F_coLAS_test1/main.c
@@ -0,0 +1,14 @@
+
+/*
+ * File:   main.c
+ * Author: shane
+ *
+ * Created on 08 April 2020, 15:45
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
